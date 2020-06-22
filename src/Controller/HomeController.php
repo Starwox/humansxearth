d989aeb4a838eca4fe9fcc530942ebb95df513a4
Yannis <rezzoug.yannis@gmail.com>
2020-06-22 12:14:16 +0200

    test json

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,24 @@
 namespace App\Controller;
 
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class HomeController
 {
+
+
+     /**
+      * @Route("/home")
+      */
+    public function number(): JsonResponse
+    {
+        // Exemple for Json Response
+        $json = file_get_contents("/Users/starwox/Desktop/humans_planets/src/JsonFile/example.json");
+        $data = json_decode($json, true);
+
+        return new JsonResponse($data);
+    }
 
 }
