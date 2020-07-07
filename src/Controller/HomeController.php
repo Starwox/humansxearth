c6d14102cce138004479167c14f9bc9e89c97ede
Yannis <rezzoug.yannis@gmail.com>
2020-07-07 22:30:19 +0200

    steps

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -779,36 +779,154 @@ class HomeController extends AbstractController
     }
 
     /**
-     * @Route("/step")
+     * @Route("/generate/data/steps")
      */
-    public function step(): Response
+    public function steps(): Response
     {
+        $repository = $this->getDoctrine()->getRepository(Tag::class);
+
+
+        // Déchets
+        $tagDechets = $repository->find(1);
+
         $step = new Step();
-        $step->setName('La pollution');
+        $step->setName("La plage");
+        $step->setTag($tagDechets);
 
         $step2 = new Step();
-        $step2->setName("Au travail");
+        $step2->setName("Les emballages");
+        $step2->setTag($tagDechets);
 
         $step3 = new Step();
-        $step3->setName("Le streaming");
+        $step3->setName("Le ramassage");
+        $step3->setTag($tagDechets);
 
         $step4 = new Step();
-        $step4->setName('Les données');
+        $step4->setName("Le recyclage");
+        $step4->setTag($tagDechets);
 
         $step5 = new Step();
-        $step5->setName("L'obsolescence");
+        $step5->setName("Se limiter");
+        $step5->setTag($tagDechets);
 
         $step6 = new Step();
-        $step6->setName('Le téléphone');
+        $step6->setName("Les cottons tiges");
+        $step6->setTag($tagDechets);
 
-        $repository = $this->getDoctrine()->getRepository(Tag::class);
-        $tag = $repository->find(5);
-        $step->setTag($tag);
-        $step2->setTag($tag);
-        $step3->setTag($tag);
-        $step4->setTag($tag);
-        $step5->setTag($tag);
-        $step6->setTag($tag);
+
+        // Style de Vie
+
+        $tagSDV = $repository->find(11);
+
+        $step7 = new Step();
+        $step7->setName("Se déplacer");
+        $step7->setTag($tagSDV);
+
+        $step8 = new Step();
+        $step8->setName("S'habiller");
+        $step8->setTag($tagSDV);
+
+        $step9 = new Step();
+        $step9->setName("La créativité");
+        $step9->setTag($tagSDV);
+
+        $step10 = new Step();
+        $step10->setName("La soif");
+        $step10->setTag($tagSDV);
+
+        $step11 = new Step();
+        $step11->setName("Le sport");
+        $step11->setTag($tagSDV);
+
+        $step12 = new Step();
+        $step12->setName("La fête");
+        $step12->setTag($tagSDV);
+
+
+        // A la maison
+
+        $tagHome = $repository->find(21);
+
+        $step13 = new Step();
+        $step13->setName("La viande");
+        $step13->setTag($tagHome);
+
+        $step14 = new Step();
+        $step14->setName("Les saisons");
+        $step14->setTag($tagHome);
+
+        $step15 = new Step();
+        $step15->setName("Acheter local");
+        $step15->setTag($tagHome);
+
+        $step16 = new Step();
+        $step16->setName("Le nettoyage");
+        $step16->setTag($tagHome);
+
+        $step17 = new Step();
+        $step17->setName("L'hygiène");
+        $step17->setTag($tagHome);
+
+        $step18 = new Step();
+        $step18->setName("La cuisine");
+        $step18->setTag($tagHome);
+
+        // Consommation
+
+        $tagConso = $repository->find(31);
+
+        $step19 = new Step();
+        $step19->setName("Le chauffage");
+        $step19->setTag($tagConso);
+
+        $step20 = new Step();
+        $step20->setName("La nourriture");
+        $step20->setTag($tagConso);
+
+        $step21 = new Step();
+        $step21->setName("L'électricité");
+        $step21->setTag($tagConso);
+
+        $step22 = new Step();
+        $step22->setName("Se laver");
+        $step22->setTag($tagConso);
+
+        $step23 = new Step();
+        $step23->setName("L'eau");
+        $step23->setTag($tagConso);
+
+        $step24 = new Step();
+        $step24->setName("Le composteur");
+        $step24->setTag($tagConso);
+
+        // Numérique
+
+        $tagNum = $repository->find(41);
+
+        $step25 = new Step();
+        $step25->setName("La pollution");
+        $step25->setTag($tagNum);
+
+        $step26 = new Step();
+        $step26->setName("Au travail");
+        $step26->setTag($tagNum);
+
+        $step27 = new Step();
+        $step27->setName("Le streaming");
+        $step27->setTag($tagNum);
+
+        $step28 = new Step();
+        $step28->setName("Les données");
+        $step28->setTag($tagNum);
+
+        $step29 = new Step();
+        $step29->setName("L'obsolescence");
+        $step29->setTag($tagNum);
+
+        $step30 = new Step();
+        $step30->setName("Le téléphone");
+        $step30->setTag($tagNum);
+
 
 
         $entityManager = $this->getDoctrine()->getManager();
@@ -818,15 +936,37 @@ class HomeController extends AbstractController
         $entityManager->persist($step4);
         $entityManager->persist($step5);
         $entityManager->persist($step6);
+        $entityManager->persist($step7);
+        $entityManager->persist($step8);
+        $entityManager->persist($step9);
+        $entityManager->persist($step10);
+        $entityManager->persist($step11);
+        $entityManager->persist($step12);
+        $entityManager->persist($step13);
+        $entityManager->persist($step14);
+        $entityManager->persist($step15);
+        $entityManager->persist($step16);
+        $entityManager->persist($step17);
+        $entityManager->persist($step18);
+        $entityManager->persist($step19);
+        $entityManager->persist($step20);
+        $entityManager->persist($step21);
+        $entityManager->persist($step22);
+        $entityManager->persist($step23);
+        $entityManager->persist($step24);
+        $entityManager->persist($step25);
+        $entityManager->persist($step26);
+        $entityManager->persist($step27);
+        $entityManager->persist($step28);
+        $entityManager->persist($step29);
+        $entityManager->persist($step30);
+
+
         $entityManager->flush();
 
 
         return new Response(
-            'Saved new news with id: '.$step->getId() .
-            'Saved new news with id: '.$step2->getId() .
-            'Saved new news with id: '.$step3->getId() .
-            'Saved new news with id: '.$step4->getId() .
-            'Saved new news with id: '.$step5->getId()
+            'Saved new STEPS'
         );
     }
 
