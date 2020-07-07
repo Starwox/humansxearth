a5c4bb3b3de4d7c8b94d698d8b358b0b8a2c41b2
Yannis <rezzoug.yannis@gmail.com>
2020-07-07 21:59:57 +0200

    methods

--- src/Controller/JsonController.php
+++ src/Controller/JsonController.php
@@ -123,13 +123,13 @@ class JsonController extends AbstractController
         $repo = $this->getDoctrine()->getRepository(User::class);
         $checker = $repo->findBy(['email' => $email]);
 
-        //$encoded = password_verify($password, $checker->getPassword());
+        $encoded = password_verify($password, $checker->getPassword());
 
 
         if (empty($checker)) {
             return new JsonResponse(["success" => "no"]);
         }
-        die();
+        dump($encoded);die();
 
         return new JsonResponse([
             'success' => 'yes',
