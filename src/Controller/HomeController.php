6bbb6d3990f338f7372ebb1ec99c8102b881757d
Yannis <rezzoug.yannis@gmail.com>
2020-07-08 01:17:19 +0200

    chall 26

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1602,19 +1602,19 @@ class HomeController extends AbstractController
     public function challenge(): Response
     {
 
-        $tips = $this->getDoctrine()->getRepository(Tips::class)->find(1);
-        $tips2 = $this->getDoctrine()->getRepository(Tips::class)->find(11);
-        $tips3 = $this->getDoctrine()->getRepository(Tips::class)->find(21);
+        $tips = $this->getDoctrine()->getRepository(Tips::class)->find(811);
+        $tips2 = $this->getDoctrine()->getRepository(Tips::class)->find(821);
+        $tips3 = $this->getDoctrine()->getRepository(Tips::class)->find(831);
 
-        $step = $this->getDoctrine()->getRepository(Step::class)->find(1);
+        $step = $this->getDoctrine()->getRepository(Step::class)->find(271);
 
-        $news = $this->getDoctrine()->getRepository(News::class)->find(601);
-        $news2 = $this->getDoctrine()->getRepository(News::class)->find(611);
+        $news = $this->getDoctrine()->getRepository(News::class)->find(1141);
+        $news2 = $this->getDoctrine()->getRepository(News::class)->find(1151);
 
         $chall = new Challenge();
 
-        $chall->setContent("Reste écolo à la plage !");
-        $chall->setToKnow("67.423 mégots de cigarettes, 25.693 sacs plastiques ont été ramassé sur 5 sites espagnols et français en 2015");
+        $chall->setContent("Limiter le stockage de données");
+        $chall->setToKnow("Les Data Centers représentent à eux seuls 17% de l’empreinte carbone de la technologie");
         $chall->setStep($step);
         $chall->addTip($tips);
         $chall->addTip($tips2);
