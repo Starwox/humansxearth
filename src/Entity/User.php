36eb24c0361017965992bad8043e83e787d025a2
Yannis <rezzoug.yannis@gmail.com>
2020-07-08 11:39:00 +0200

    header apache

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,7 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Entity(repositoryClass=App\Repository\UserRepository)
  */
 class User
 {
