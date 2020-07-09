15dec638f8ae0db95a9ed574ea187a0a45995e0c
Yannis <rezzoug.yannis@gmail.com>
2020-07-09 10:35:27 +0200

    cors problem

--- public/index.php
+++ public/index.php
@@ -22,14 +22,24 @@ if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
     Request::setTrustedHosts([$trustedHosts]);
 }
-
+/*
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Methods: GET, POST, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+*/
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
+
+$request->headers->set('Content-Type', 'application/json');
+$request->headers->set('Access-Control-Allow-Origin', '*');
+$request->headers->set('Access-Control-Allow-Credentials', 'true');
+$request->headers->set('Access-Control-Max-Age', '60');
+$request->headers->set('Access-Control-Allow-Headers', 'AccountKey,x-requested-with, Content-Type, origin, authorization, accept, client-security-token, host, date, cookie, cookie2');
+$request->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+
+
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
