0920e386b83c3fda81a1ee902b191d3b00a730d4
Yannis <rezzoug.yannis@gmail.com>
2020-07-09 17:15:28 +0200

    update json

--- src/Controller/JsonController.php
+++ src/Controller/JsonController.php
@@ -396,8 +396,6 @@ class JsonController extends AbstractController
         $repository = $this->getDoctrine()->getRepository(User::class);
         $object = $repository->find($id);
 
-        dump($object);die();
-
         $defaultContext = [
             AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object, $format, $context) {
                 return $object;
